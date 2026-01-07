ea729ba00b8d4080202826db188b9dc4783f86a2
datnguyen-lovepop <dat.nguyennguyenthanh@lovepopvn.com>
2026-01-07 14:05:07 +0700

    Update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://sonynguyen8.github.io',
+	base: '/sony-spaceship',
 	integrations: [react(), mdx(), sitemap()],
 });
+	
