1b42b79e27525a6a0c8c126c95919a370526da05
datnguyen-lovepop <dat.nguyennguyenthanh@lovepopvn.com>
2026-01-07 20:56:03 +0700

    Revert astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sonynguyen8.github.io',
-	base: '/sony-spaceship',
+	// base: '/sony-spaceship',
 	integrations: [react(), mdx(), sitemap()],
 });
 	
